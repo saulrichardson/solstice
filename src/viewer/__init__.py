eb608f077876030505e6351b6b21931102752173
saulrichardson <saulsido101@gmail.com>
2025-07-30 19:44:12 -0400

    updates

--- src/viewer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Document viewer module for creating unified views of extracted documents."""
-
-from .aggregator import DocumentAggregator
-from .html_generator import UnifiedHTMLGenerator
-
-__all__ = ["DocumentAggregator", "UnifiedHTMLGenerator"]
